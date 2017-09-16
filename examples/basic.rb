550050969e2dca47380dfe545835f7fc22e02409
Sebastian Skałacki <skalee@gmail.com>
2017-09-16 16:05:55 +0200

    Fix spacing inside literal hashes
    According to Ribose OSS Style Guide, they should be like this.

--- examples/basic.rb
+++ examples/basic.rb
@@ -1,5 +1,5 @@
 class MyModel
-  validates :favourite_website_url, url: {resolvability: :retrievable}
-  validates :resume_cv_url, url: {resolvability: :reachable}
-  validates :friend_website_url, url: {scheme: %w[http https ftp]}
+  validates :favourite_website_url, url: { resolvability: :retrievable }
+  validates :resume_cv_url, url: { resolvability: :reachable }
+  validates :friend_website_url, url: { scheme: %w[http https ftp] }
 end
