e83f088dc0f5b1ec87818ddaeb2e83f89c71ab09
Sebastian Skałacki <skalee@gmail.com>
2017-09-27 03:36:05 +0200

    Rename validation helper method
    Since the validator class has been renamed, the helper method should be
    renamed accordingly.

--- lib/validators/helper_methods.rb
+++ lib/validators/helper_methods.rb
@@ -6,9 +6,9 @@ module ActiveModel
       # Encapsulates the pattern of wanting to validate an URL.
       #
       #   class Post < ActiveRecord::Base
-      #     validates_url_of :permalink
+      #     validates_uri_format_of :permalink
       #   end
-      def validates_url_of(*attr_names)
+      def validates_uri_format_of(*attr_names)
         validates_with UriFormatValidator, _merge_attributes(attr_names)
       end
     end
