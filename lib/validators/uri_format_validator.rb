51e216fcb7dd24aba02b835d71df02dc6213170d
Sebastian Skałacki <skalee@gmail.com>
2017-09-27 15:13:56 +0200

    Simplify long method

--- lib/validators/uri_format_validator.rb
+++ lib/validators/uri_format_validator.rb
@@ -199,7 +199,7 @@ module ActiveModel
 
       def use_https?(url)
         url.scheme == "https" ||
-          (url.scheme == nil && @schemes.is_a?(Array) && @schemes.include?("https"))
+          (url.scheme.nil? && @schemes.try(:include?, "https"))
       end
 
       def generic_validate_retrievable(url)
