45f6eba1975f9c0326fb1aad447203bcd684dc9b
Sebastian Skałacki <skalee@gmail.com>
2017-09-27 03:04:10 +0200

    Require support files in the spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require "bundler/setup"
 require "uri_format_validator"
 
+Dir[File.expand_path "../support/**/*.rb", __FILE__].sort.each { |f| require f }
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
