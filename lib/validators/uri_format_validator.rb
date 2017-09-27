a03b19a4362047b0497a914db107019c2100a2a8
Sebastian Skałacki <skalee@gmail.com>
2017-09-27 02:54:44 +0200

    Move HelperMethods module to separate file

--- lib/validators/uri_format_validator.rb
+++ lib/validators/uri_format_validator.rb
@@ -191,16 +191,5 @@ module ActiveModel
         nil
       end
     end
-
-    module HelperMethods
-      # Encapsulates the pattern of wanting to validate an URL.
-      #
-      #   class Post < ActiveRecord::Base
-      #     validates_url_of :permalink
-      #   end
-      def validates_url_of(*attr_names)
-        validates_with UriFormatValidator, _merge_attributes(attr_names)
-      end
-    end
   end
 end
---
+++ spec/helper_methods_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+
+describe ActiveModel::Validations::HelperMethods do
+  let(:post) { Post.new }
+
+  it "provides alternative, old-fashioned way to set validations" do
+    Post.validates_url_of :url
+
+    post.url = "http://google"
+    expect(post).not_to be_valid
+    post.url = "http://google.com"
+    expect(post).to be_valid
+  end
+end
