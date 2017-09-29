f43366beb5bb7ab2827b1d5568e05e4ff16fb249
Sebastian Skałacki <skalee@gmail.com>
2017-09-30 00:24:51 +0200

    Write a module to aggregate validators
    Include that module into ActiveModel::Validations so that validators are
    available for use.

--- lib/uri_format_validator.rb
+++ lib/uri_format_validator.rb
@@ -3,10 +3,11 @@
 
 require "uri_format_validator/version"
 require "uri_format_validator/localization"
-require "uri_format_validator/validators/uri_format_validator"
-require "uri_format_validator/validators/helper_methods"
+require "uri_format_validator/validators"
 
 # This is a placeholder...
 module UriFormatValidator
   # Your code goes here...
 end
+
+ActiveModel::Validations.include UriFormatValidator::Validators
---
+++ lib/uri_format_validator/validators.rb
@@ -0,0 +1,16 @@
+# (c) Copyright 2017 Ribose Inc.
+#
+
+require "uri_format_validator/validators/uri_format_validator"
+require "uri_format_validator/validators/helper_methods"
+
+module UriFormatValidator
+  module Validators
+    extend ActiveSupport::Concern
+
+    included do
+      extend  HelperMethods
+      include HelperMethods
+    end
+  end
+end
