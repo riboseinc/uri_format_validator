8b3d24f234f373acbcec8f893f05b7e013c692f2
Sebastian Skałacki <skalee@gmail.com>
2017-09-30 02:51:15 +0200

    Update examples to use the current validator name

--- examples/basic.rb
+++ examples/basic.rb
@@ -1,5 +1,5 @@
 class MyModel
-  validates :favourite_website_url, url: { resolvability: :retrievable }
-  validates :resume_cv_url, url: { resolvability: :reachable }
-  validates :friend_website_url, url: { scheme: %w[http https ftp] }
+  validates :favourite_website_url, uri_format: { resolvability: :retrievable }
+  validates :resume_cv_url, uri_format: { resolvability: :reachable }
+  validates :friend_website_url, uri_format: { scheme: %w[http https ftp] }
 end
