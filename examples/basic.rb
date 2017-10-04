4255fcb317863e035e5e471caff390acccb88ee0
Sebastian Skałacki <skalee@gmail.com>
2017-10-04 03:00:18 +0200

    Get rid of examples directory
    Examples have been superseded with Cucumber-based documentation.

--- examples/basic.rb
+++
@@ -1,5 +0,0 @@
-class MyModel
-  validates :favourite_website_url, uri_format: { resolvability: :retrievable }
-  validates :resume_cv_url, uri_format: { resolvability: :reachable }
-  validates :friend_website_url, uri_format: { scheme: %w[http https ftp] }
-end
