24878c9b829dbf091aa80fda5db03f62d59b4e30
Sebastian Skałacki <skalee@gmail.com>
2017-10-14 05:31:24 +0200

    Rename model validator

--- lib/uri_format_validator/validators.rb
+++ lib/uri_format_validator/validators.rb
@@ -1,7 +1,7 @@
 # (c) Copyright 2017 Ribose Inc.
 #
 
-require "uri_format_validator/validators/uri_format_validator"
+require "uri_format_validator/validators/uri_validator"
 require "uri_format_validator/validators/helper_methods"
 
 module UriFormatValidator
