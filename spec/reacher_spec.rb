e909e0045b100e3f135a568f6c67be93c2ee682e
Sebastian Skałacki <skalee@gmail.com>
2017-10-15 09:53:31 +0200

    Rework specs for Reacher
    This way upcoming tests for Reacher#resolvable? will be much easier to
    write.

--- spec/reacher_spec.rb
+++ spec/reacher_spec.rb
@@ -5,29 +5,33 @@ require "spec_helper"
 
 RSpec.describe UriFormatValidator::Reacher do
   let(:url_s) { "http://host.example.test/resource" }
-  let(:url) { URI(url_s) }
-  let(:reacher) { described_class.new(url) }
 
   it "is initialized with an URL" do
     expect(described_class.new(double(URI))).to be_instance_of(described_class)
   end
 
   describe "#retrievable?" do
-    subject { reacher.method :retrievable? }
+    subject { described_class.instance_method :retrievable? }
 
     it "returns true when the response is HTTP 2xx" do
       stub_request(:head, url_s).to_return(status: 200)
-      expect(subject.call).to be(true)
+      expect(retval_for(url_s)).to be(true)
     end
 
     it "returns false when the response is HTTP 4xx" do
       stub_request(:head, url_s).to_return(status: 403)
-      expect(subject.call).to be(false)
+      expect(retval_for(url_s)).to be(false)
     end
 
     it "returns false when the response is HTTP 5xx" do
       stub_request(:head, url_s).to_return(status: 500)
-      expect(subject.call).to be(false)
+      expect(retval_for(url_s)).to be(false)
     end
+  end
+
+  def retval_for(url)
+    instance = described_class.new(URI(url))
+    bound_method = subject.bind(instance)
+    bound_method.call
   end
 end
