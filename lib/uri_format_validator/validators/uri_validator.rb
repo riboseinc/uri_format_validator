94e7bb5b661e99226bca3f61421e3f16ae332513
Sebastian Skałacki <skalee@gmail.com>
2017-10-26 11:10:23 +0200

    Re-implement the :scheme validation option
    The :scheme option constraints schemes allowed for given URI.

--- lib/uri_format_validator/validators/uri_validator.rb
+++ lib/uri_format_validator/validators/uri_validator.rb
@@ -33,7 +33,7 @@ module UriFormatValidator
         uri = string_to_uri(uri_string)
         invalid unless uri
 
-        validate_against_options(uri, :retrievable)
+        validate_against_options(uri, :scheme, :retrievable)
       end
 
       def string_to_uri(uri_string)
@@ -55,6 +55,11 @@ module UriFormatValidator
           next unless options.key?(option_name)
           send(:"validate_#{option_name}", options[option_name], uri)
         end
+      end
+
+      def validate_scheme(scheme_or_schemes, uri)
+        schemes = Array.wrap(scheme_or_schemes)
+        invalid unless uri.scheme.in?(schemes)
       end
 
       def validate_retrievable(option, uri)
